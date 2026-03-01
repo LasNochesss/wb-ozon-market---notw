1bd14361c9f853b42557146f060cc221b047df23
LasNochesss <166229622+LasNochesss@users.noreply.github.com>
2026-03-01 19:46:55 +0300

    feat: localize UI to Russian and set ru-RU culture

--- src/UI/App.xaml.cs
+++ src/UI/App.xaml.cs
@@ -1,5 +1,18 @@
+using System.Globalization;
+using System.Threading;
 using System.Windows;
 
 namespace UI;
 
-public partial class App : Application { }
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        var ruCulture = new CultureInfo("ru-RU");
+        Thread.CurrentThread.CurrentCulture = ruCulture;
+        Thread.CurrentThread.CurrentUICulture = ruCulture;
+        CultureInfo.DefaultThreadCurrentCulture = ruCulture;
+        CultureInfo.DefaultThreadCurrentUICulture = ruCulture;
+        base.OnStartup(e);
+    }
+}
